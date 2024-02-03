50f087ce1a4b1bf470e32f79b31999039f2bd586
kesselleg <95750431+kesselleg@users.noreply.github.com>
2024-02-03 03:57:00 -0800

    first work

---
+++ html/task.js
@@ -0,0 +1,6 @@
+
+const date = new Date()
+const today = date.getDay()
+
+const day = ["Sunday", "Monday", "Tuesday", "Wenesday", "Thurday", "Friday" ,"Saturday"]
+console.log("Today is : " +day[today]+ ".")
---
+++ work2.js
@@ -0,0 +1,11 @@
+
+
+function integernumber (array){
+    return Math.max(...array)
+}
+
+const largenumber = [34,65,7]
+
+console.log(integernumber(largenumber))
+
+
